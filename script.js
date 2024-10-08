0e2f71124c7fadd31fb334e84a6ed97fb59672df
Sabrin16 <sabrin.salem@hotmail.se>
2024-10-08 20:01:06 +0200

    jobbat med wariabler

---
+++ script.js
@@ -0,0 +1,105 @@
+
+// wariabel = någonting som vi lgrar information i
+
+let firstName = "sabrin";
+const lastName = 'salem';
+const age = 37;
+
+// firstName =  'salem';
+// lastName = 'salem';
+
+// console.log(firstName);
+// console.log(lastName);
+
+
+/*
+string 
+number 
+booleon 
+null  
+underfiend 
+object 
+arroy 
+*/
+
+
+// let text = 'Hej jag heter '+ firstName +' sabrin och är 37 år gammal'
+// text = "Hej jag heter sabrin och är "+ age +" 37 år gammal"
+
+// let text = `Hej jag heter ${firstName} och är ${age} år gammal`
+
+// console.log(text)
+
+// let num1 = 5;
+// let num2 = '7';
+
+// console.log(num1 + num2)
+
+// let randomnr = Math.random() * 100
+
+// randomnr = Math.round(randomnr);
+// console.log(randomnr);
+
+// num1 = num1 + 1;
+// num1 += 4;
+// num1 ++;
+
+// num1 --;
+// console.log(num1)
+
+
+
+
+
+
+
+// let num1 = 5;
+// let num2 = '5';
+
+// const isloggedin = true;
+
+// console.log(num1 === num2);
+
+// console.log(isloggedin == true)
+// const string = 'a'
+// const number = 0
+
+// if(number) {
+// console.log('det finns ett truthy värde') 
+// }
+
+
+// let name = null;
+
+// name = 'sabrin salem';
+
+// if(name != null) {
+// console.log(`welcome $(name)`)
+// }
+
+
+const user = {
+firstName: 'sabrin',
+lastName: 'salem',
+age 12,
+isvalidated: false,
+address: {
+  street: 'gatan',
+  nr: 12,
+  postaicode: 71234,
+  city: 'västerås'
+ },
+ phonenumber: 0729812301,
+}
+
+// console.log(user)
+
+
+
+const user = {
+  {name: 'sabrin', age: 37, isactrive: true},
+  {name: 'nisse', age: 30, isactrive: false} 
+  {name: 'hans', age: 50, isactrive: false},
+}
+
+console.log(user)
